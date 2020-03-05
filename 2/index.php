41516334863b00343390ce0437f261db3c760d6a
ksush <ksushka.e@mail.ru>
2020-03-05 16:11:04 +0300

    ch in hw3

--- 2/index.php
+++ 2/index.php
@@ -5,34 +5,7 @@ if (isset($_POST["send"])) {
     $text = (string) $_POST["text"];
     $pos = 0;
     $arr = array(0);
-//    function changeText($text){
-//        $changes = 0;
-//        for ($i=0; $i<strlen($text); ++$i){
-//            switch ($text[$i]){
-//                case "h":
-//                    $text[$i] = 4;
-//                    $changes++;
-//                    break;
-//                case "i":
-//                    $text[$i] = 1;
-//                    $changes++;
-//                    break;
-//                case "e":
-//                    $text[$i] = 3;
-//                    $changes++;
-//                    break;
-//                case "o":
-//                    $text[$i] = 0;
-//                    $changes++;
-//                    break;
-//            }
-//        }
-//        return [$text, $changes];
-//    }
-//    print("New text: ");
-//    print (changeText($text)[0]);
-//    print(" and number of changes: ");
-//    print(changeText($text)[1]);
+
     function changeText($text){
         for ($i=0; $i<strlen($text); ++$i){
             yield $text[$i];
@@ -64,6 +37,7 @@ if (isset($_POST["send"])) {
                 echo($item);
                 break;
         }
+
     }
 //    echo "return = ".$gen->getReturn();
     print("<br/>");
--- 3/index.php
+++ 3/index.php
@@ -7,17 +7,27 @@ if (isset($_POST["send"])) {
     $arr=explode($del,$text);
     $st = array();
     $sec = array();
+    $st_start = array();
     for($i=0; $i<count($arr); $i++){
+        $st_start[$i] = explode(" ", $arr[$i]);
         $st[$i] = explode(" ", $arr[$i]);
         shuffle($st[$i]);
-//        print_r($st[$i]);
+        $mas = array(count($arr)+$i => $st_start[$i][1]);
         $el = array($i => $st[$i][1]);
         $sec+= $el;
+        $sec+= $mas;
     }
     natcasesort($sec);
     for($i=0; $i<count($sec); $i++){
-        print(implode(" ",$st[key($sec)]));
-        print("<br/>");
+        $pos = key($sec);
+        if($pos < count($sec)/2){
+            print(implode(" ",$st[$pos]));
+            print("<br/>");
+        }
+        else{
+            print(implode(" ",$st_start[$pos - count($sec)/2]));
+            print("<br/>");
+        }
         next($sec);
     }
 
